3a9577aa7d778881960b5a9011b58e2bb85628c6
Mehran Hosseini <mehran.hosseini@ebcont.com>
2022-06-01 15:57:49 +0200

    WIP

---
+++ monolith/src/main/java/org/springframework/samples/petclinic/vets/VetClient.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.vets;
+
+
+import org.checkerframework.checker.units.qual.Temperature;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+public class VetClient {
+
+//    public Collection<VetsDTO> getVets() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.getForObject("http://localhost:8080/vetsnew", String.class);
+//    }
+}
